9ccc30ed7090e70541e15eacf1c4c67c0e6d8c11
Henrique Greghi <henriquevsg@gmail.com>
2022-08-31 11:10:15 -0300

    Tocando musicas

---
+++ TocarMusica/EscolhaMusica.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TocarMusica
+{
+    class EscolhaMusica
+    {
+        private int Valor1 { get; set; }
+        private int Valor2 { get; set; }
+        private int Soma { get; set; }
+
+        public EscolhaMusica()
+        {
+        }
+
+        public EscolhaMusica(int valor1, int valor2)
+        {
+            Valor1 = valor1;
+            Valor2 = valor2;
+        }
+        public string Escolha()
+        {
+            Soma = Valor1 + Valor2;
+            switch (Soma)
+            {
+                case 0:
+                    return "PROXYCITY";
+                        
+                case 1:
+                    return "P.Y.N.G.";
+                        
+                case 2:
+                    return "DNSUEY!";
+                        
+                case 3:
+                    return "SERVERS";
+                        
+                case 4:
+                    return "HOST!";
+                        
+                case 5:
+                    return "CRIPTONIZE";
+                        
+                case 6:
+                    return "OFFLINE DAY";
+                        
+                case 7:
+                    return "SALT";
+                       
+                case 8:
+                    return "ANSWER!";
+                        
+                case 9:
+                    return"RAR ?";
+                       
+                case 10:
+                    return "WIFI ANTENNAS";
+                        
+
+                default:
+                    return "Valor muito alto";
+                    
+            }
+        }
+
+    }
+}
---
+++ TocarMusica/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace TocarMusica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Entre com quantos testes pretende fazer :");
+            int testes = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escolha a música: ");
+            Console.WriteLine("0 - PROXYCITY");
+            Console.WriteLine("1 - P.Y.N.G. ");
+            Console.WriteLine("2 - DNSUEY!");
+            Console.WriteLine("3 - SERVERS");
+            Console.WriteLine("4 - HOST!");
+            Console.WriteLine("5 - CRIPTONIZE");
+            Console.WriteLine("6 - OFFLINE DAY");
+            Console.WriteLine("7 - SALT");
+            Console.WriteLine("8 - ANSWER!");
+            Console.WriteLine("9 - RAR?");
+            Console.WriteLine("10 - WIFI ANTENNAS");
+            Console.WriteLine();
+            Console.Write("Digite 2 números que somados dão a sua escolha: ");
+
+
+            try
+            {
+                for (int i = 1; i < testes + 1; i++)
+                {
+
+                    string[] valores = Console.ReadLine().Split(' ');
+                    int valor1 = int.Parse(valores[0]);
+                    int valor2 = int.Parse(valores[1]);
+
+                    EscolhaMusica escolha = new EscolhaMusica(valor1, valor2);
+
+                    Console.WriteLine(escolha.Escolha());
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(" ");
+            }
+
+
+
+            Console.WriteLine("Precione qualquer tecla para fechar: ");
+
+        }
+    }
+}
